32413c6e91cb656c78c6ccdead17adc3ad9b80cb
Noura Alowayid <132269252+NouraOwa@users.noreply.github.com>
2023-05-22 09:58:05 +0300

    add DesignsPic

---
+++ FiveModels/FiveModels/Models/TripModel.swift
@@ -0,0 +1,8 @@
+//
+//  TripModel.swift
+//  FiveModels
+//
+//  Created by Noura Alowayid on 02/11/1444 AH.
+//
+
+import Foundation
---
+++ FiveModels/FiveModels/Trips.swift
@@ -0,0 +1,20 @@
+//
+//  Trips.swift
+//  FiveModels
+//
+//  Created by Noura Alowayid on 02/11/1444 AH.
+//
+
+import SwiftUI
+
+struct Trips: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Trips_Previews: PreviewProvider {
+    static var previews: some View {
+        Trips()
+    }
+}
